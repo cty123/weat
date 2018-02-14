e860a45fea6130477b5c6447070ee17ce8032e02
seanbecker15 <sean.becker15@gmail.com>
2018-02-14 17:13:56 -0500

    Added selectors for list view

--- Weat/Tabs/FeedViewController.swift
+++ Weat/Tabs/FeedViewController.swift
@@ -3,9 +3,27 @@ import UIKit
 import FBSDKCoreKit
 
 class  FeedViewController: UIViewController {
+    
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            print("Everyone")
+            // Move to everyone list view
+        case 1:
+            print("Friends")
+            // Move to friends list view
+        default:
+            print("You")
+            // Move to your list view
+            break;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         //let access_token = FBSDKAccessToken.current().tokenString!
         //let user = User.getUserInfo(params: ["access_token":access_token])
         
