169d99c8727e0d075358ece2f2aab7b244456ef5
seanbecker15 <sean.becker15@gmail.com>
2018-02-15 15:38:11 -0500

    Example query

--- Weat/Tabs/FeedViewController.swift
+++ Weat/Tabs/FeedViewController.swift
@@ -29,5 +29,28 @@ class  FeedViewController: UIViewController {
         
     }
     
+    /*static func getYouFeed() {
+        let url = "http://127.0.0.1:3000/user/profile"
+        let user:User = User()
+        Alamofire.request(url, method:.get, parameters:params).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                let user:User = User()
+                user.name = json["user"]["name"].string
+                user.location = json["user"]["location"].string
+                user.email = json["user"]["email"].string
+                user.id = json["user"]["id"].int
+                user.privacy = json["user"]["email"].int
+                user.phone = json["user"]["phone"].string
+                print(user)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        return user
+    }*/
+    
 }
 
