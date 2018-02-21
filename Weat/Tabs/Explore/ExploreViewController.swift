218fb4e3cd33f1d9312702150057edd05ebf4c0e
seanbecker15 <sean.becker15@gmail.com>
2018-02-21 02:14:47 -0500

    Made segmented controller and sub views for explore page

--- Weat/Tabs/Explore/ExploreViewController.swift
+++ Weat/Tabs/Explore/ExploreViewController.swift
@@ -9,10 +9,58 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-
+    
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var seg: UISegmentedControl!
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        
+        var nextController: UIViewController
+        var prevController: UIViewController
+        // Choose child
+        if(sender.selectedSegmentIndex == 0) {
+            nextController = self.storyboard?.instantiateViewController(withIdentifier: "Map") as! MapViewController
+            prevController = self.storyboard?.instantiateViewController(withIdentifier: "List") as! ListViewController
+        } else {
+            nextController = self.storyboard?.instantiateViewController(withIdentifier: "List") as! ListViewController
+            prevController = self.storyboard?.instantiateViewController(withIdentifier: "Map") as! MapViewController
+        }
+        
+        // Figure out how to make transition
+        
+        // Add child to subview
+        self.addChildViewController(nextController)
+        self.view.addSubview(nextController.view)
+        
+        // Set up new child
+        nextController.view.frame = self.view.bounds
+        nextController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Notify new child
+        nextController.didMove(toParentViewController: self)
+        
+        // Remove old child
+        prevController.willMove(toParentViewController: nil)
+        
+        prevController.view.removeFromSuperview()
+        prevController.removeFromParentViewController()
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Change segmented control looks
+        seg.setup(segmentNames: ["Map", "List"], color: UIColor.orange)
+        
+        // Show maps view
+        let initialController = self.storyboard?.instantiateViewController(withIdentifier: "Map") as! MapViewController
+        self.addChildViewController(initialController)
+        self.view.addSubview(initialController.view)
+        initialController.view.frame = self.view.bounds
+        initialController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        initialController.didMove(toParentViewController: self)
+        
+        
         // Do any additional setup after loading the view.
     }
 
