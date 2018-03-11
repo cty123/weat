d18b8307e847c06dda698a1c758fc5aec31744a1
cty123 <chen2216@purdue.edu>
2018-03-10 22:14:05 -0500

    Implement get restaurant menu and get restaurant comment function, they are both in Restaurant class

---
+++ Weat/models/Menu_item.swift
@@ -0,0 +1,15 @@
+//
+//  Menu_item.swift
+//  Weat
+//
+//  Created by ctydw on 3/10/18.
+//  Copyright © 2018 Weat. All rights reserved.
+//
+
+import Foundation
+class Menu_item{
+    var name:String?
+    var category: String?
+    var rating:Rating?
+    var id: Int?
+}
