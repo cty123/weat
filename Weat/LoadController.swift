d4ae4901a9dbaee4f2e9022d2fa1587eed75cd25
seanbecker15 <sean.becker15@gmail.com>
2018-03-20 16:57:58 -0400

    Added fbauth2 to prevent error on login; added error message to login

--- Weat/LoadController.swift
+++ Weat/LoadController.swift
@@ -6,7 +6,6 @@ import Alamofire
 import SwiftyJSON
 
 class LoadController: UIViewController {
-    var isLoggedIn = false
     var latitude: Double?   // Used to store location for explore view
     var longitude: Double?  // Used to store location for explore view
     
@@ -20,45 +19,16 @@ class LoadController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         if(FBSDKAccessToken.current() != nil) {
-            let token = FBSDKAccessToken.current().tokenString!
-            let url = "\(WeatAPIUrl)/auth/facebook/token?access_token=\(String(describing: token))"
-            print("Getting url \(String(describing: url))")
-            Alamofire.request(url, method: .get).validate().responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    UserDefaults.standard.set(json["id"].int, forKey: "id")
-                    UserDefaults.standard.set(json["name"].string, forKey: "name")
-                    UserDefaults.standard.set(json["email"].string, forKey: "email")
-                    UserDefaults.standard.set(json["location"].string, forKey: "location")
-                    UserDefaults.standard.set(json["privacy"].int, forKey: "privacy")
-                    UserDefaults.standard.set(json["phone"].string, forKey: "phone")
-                    self.isLoggedIn = true
-                    print("performing segue from loadController")
+            LoginController.weatLogin(completion: { (status) in
+                if(status) {
                     self.performSegue(withIdentifier: "enterApp", sender: self)
-                    // Testing pulling friends
-                    /*FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "friends"]).start(completionHandler: { (connection, result, error) -> Void in
-                     if (error == nil){
-                     print(result as Any)
-                     } else {
-                     print(error as Any)
-                     }
-                     })*/
-                case .failure(let error):
-                    print("load controller error: \(error)")
-                    self.isLoggedIn = false
+                } else {
+                    print("LoginController error: Can't access server. Check that it's up.")
                     self.performSegue(withIdentifier: "enterLogin", sender: self)
                 }
-            }
+            })
         } else {
             self.performSegue(withIdentifier: "enterLogin", sender: self)
-            isLoggedIn = false
-        }
-        if (isLoggedIn)
-        {
-     //       performSegue(withIdentifier: "enterApp", sender: self)
-        }else{
-    //        performSegue(withIdentifier: "enterLogin", sender: self)
         }
     }
 }
