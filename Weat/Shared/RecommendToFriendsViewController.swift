2c91523cd583d8f7fb97c337cde453c71fd9afe5
cty123 <chen2216@purdue.edu>
2018-03-26 11:46:44 -0400

    Fix the extra comma in send recommendation function

--- Weat/Shared/RecommendToFriendsViewController.swift
+++ Weat/Shared/RecommendToFriendsViewController.swift
@@ -33,7 +33,7 @@ class RecommendToFriendsViewController: UIViewController, UITableViewDelegate, U
                 // if cell selected, add friend id with a comma
                 friendIDs = friendIDs + "\(friends[i].id!),"
             }
-            
+            friendIDs.removeLast()
             // increment i
             i = i + 1
         }
