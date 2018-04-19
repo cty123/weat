43976e731600d0c0d3b24fcc5911b21a691f66a7
seanbecker15 <sean.becker15@gmail.com>
2018-04-19 15:12:54 -0400

    Logout works

--- Weat/Tabs/Profile/EditProfileViewController.swift
+++ Weat/Tabs/Profile/EditProfileViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class EditProfileViewController: UIViewController {
-
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var textFieldName: UITextField!
     @IBOutlet weak var textFieldEmail: UITextField!
@@ -17,47 +17,20 @@ class EditProfileViewController: UIViewController {
     @IBOutlet weak var textFieldLocation: UITextField!
     @IBOutlet weak var switchShowArchived: UISwitch!
     
+    let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+    @IBAction func logout(_ sender: UIButton) {
+        loginManager.logOut()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as UIViewController
+        self.present(vc, animated: true, completion: nil)
+    }
     // action for closing button
     @IBAction func action(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(self.switchShowArchived.isOn , forKey: "showArchived")
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let showArchived = UserDefaults.standard.object(forKey: "showArchived"){
-            // if the key exists
-            self.switchShowArchived.isOn = showArchived as! Bool
-        } else {
-            UserDefaults.standard.set(false, forKey: "showArchived")
-            self.switchShowArchived.isOn = false
-        }
-        
-        // populate fields
-        let id = UserDefaults.standard.string(forKey: "id")
-
-        User.getUserInfo(profile_id: id!){result in
-            switch result {
-                case .success(let user):
-                    self.textFieldName.text = user.name
-                    self.textFieldEmail.text = user.email
-                    self.textFieldPhone.text = user.phone
-                    self.textFieldLocation.text = user.location
-                case .failure(let error):
-                    print("File: \(#file)")
-                    print("Line: \(#line)")
-                    print(error)
-            }
-        }
-        
-        self.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(action))
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
+    @IBAction func save(_ sender: UIButton) {
         // TODO: input validation
         var name = self.textFieldName.text
         var email = self.textFieldEmail.text
@@ -98,6 +71,43 @@ class EditProfileViewController: UIViewController {
                 print("failure")
             }
         }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let showArchived = UserDefaults.standard.object(forKey: "showArchived"){
+            // if the key exists
+            self.switchShowArchived.isOn = showArchived as! Bool
+        } else {
+            UserDefaults.standard.set(false, forKey: "showArchived")
+            self.switchShowArchived.isOn = false
+        }
+        
+        // populate fields
+        let id = UserDefaults.standard.string(forKey: "id")
+
+        User.getUserInfo(profile_id: id!){result in
+            switch result {
+                case .success(let user):
+                    self.textFieldName.text = user.name
+                    self.textFieldEmail.text = user.email
+                    self.textFieldPhone.text = user.phone
+                    self.textFieldLocation.text = user.location
+                case .failure(let error):
+                    print("File: \(#file)")
+                    print("Line: \(#line)")
+                    print(error)
+            }
+        }
+        
+        self.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(action))
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
     }
 
